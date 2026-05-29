f9f071b255cf850a3690864d4e0edd1a8227d775
swapnilnamah <namah2908@zohomail.in>
2026-05-30 00:02:05 +0530

    coorectbackendname

--- modules/parent/provider.tf
+++ modules/parent/provider.tf
@@ -1,8 +1,8 @@
 terraform {
     backend "azurerm" {
-    resource_group_name   = "aarambh"
-    storage_account_name  = "aarmabhstr"         # Must be globally unique
-    container_name        = "tfstatefile"
+    resource_group_name   = "swapnil"
+    storage_account_name  = "swapnilbackend"         # Must be globally unique
+    container_name        = "swapnilcontainer"
     key                   = "terraform.tfstate"
   }
   required_providers {
